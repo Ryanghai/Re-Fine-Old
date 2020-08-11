b10437094c20bfcd7080e1ed48549a9a08b13533
GitHubにRyanghai <68055743+Ryanghai@users.noreply.github.com>
2020-08-11 19:50:13 +0900

    ipriment login future

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+# SMTP configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.kagoya.net",
+    :port => 587,
+    :user_name => ENV["SMTP_USER"],
+    :password => ENV["SMTP_PASS"],
+    :authentication => :'login',
+    :enable_starttls_auto => true
+  }
 end
